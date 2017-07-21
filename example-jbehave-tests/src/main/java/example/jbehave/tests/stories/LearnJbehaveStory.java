2ae8cddb0834ccca568e4c522738fb0eaff24c5d
gpetrov <A6KQKZZ@mmm.com>
2017-07-21 20:09:06 +0300

    Experiment with JBehave and JUnit4 runner - not working

--- example-jbehave-tests/src/main/java/example/jbehave/tests/stories/LearnJbehaveStory.java
+++ example-jbehave-tests/src/main/java/example/jbehave/tests/stories/LearnJbehaveStory.java
@@ -1,15 +1,36 @@
 package example.jbehave.tests.stories;
 
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import example.jbehave.tests.AcceptanceTest;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.junit.JUnitStories;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import java.util.List;
+
+
+//@RunWith(SpringJUnit4ClassRunner.class)
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration("classpath:test-config.xml")
+
+@RunWith(JUnitReportingRunner.class)
+//@IncludeEngines("junit-jupiter")
 @AcceptanceTest
-public class LearnJbehaveStory extends AbstractSpringJBehaveStory {
+public class LearnJbehaveStory extends JUnitStories {
+    @Override
+    protected List<String> storyPaths() {
+        final File f = new File(LearnJbehaveStory.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        String codeLocation = f.getAbsolutePath()+"\\..\\..\\src\\main\\resources\\example\\jbehave\\tests\\stories\\";
+        System.out.println("codeLocation=" + codeLocation);
+        return new StoryFinder().findPaths(codeLocation, Arrays.asList("**/*.story"),
+                Arrays.asList(""), "file:"+codeLocation);
+    }
 }
